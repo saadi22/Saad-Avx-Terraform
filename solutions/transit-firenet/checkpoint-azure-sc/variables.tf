06711cedc672ced5ed8515340c586e3c512eda20
Saad Mirza <saadmirza@Saads-MBP.fios-router.home>
2020-08-05 13:18:33 -0500

    AZSC2

---
+++ solutions/transit-firenet/checkpoint-azure-sc/variables.tf
@@ -0,0 +1,58 @@
+variable "vpc_count" {
+  default = 2
+}
+
+variable "username" {
+  type    = string
+  default = ""
+}
+
+variable "password" {
+  type    = string
+  default = ""
+}
+
+variable "controller_ip" {
+  type    = string
+  default = ""
+}
+
+variable "cloud_type" {
+  default = 8
+}
+
+variable "hpe" {
+  default = false
+}
+
+variable "region" {
+  default = "South Central US"
+}
+
+variable "key_name" {
+  default = "avtx-key"
+}
+
+variable "azure_account_name" {
+  default = ""
+}
+
+variable "avx_transit_gw" {
+  default = "AZSC2-Transit-gw"
+}
+variable avx_gw_size {
+  default = "Standard_B2ms"
+}
+
+variable firewall_size {
+  default = "Standard_D3_v2"
+}
+
+variable fw_image_version {
+  default = "8040.900294.0593"
+}
+
+variable fw_image {
+  #default = "Check Point CloudGuard IaaS Next-Gen Firewall w. Threat Prevention & SandBlast BYOL"
+  default = "Check Point CloudGuard IaaS Single Gateway R80.40 - Pay As You Go (NGTP)"
+}    
